a0e18237f9837314611f0b2ebd26d77666b69353
hasan <halimkhanfeni7@gmail.com>
2019-05-09 23:59:37 +0600

    Arranna psd to html

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// You can also use "$(window).load(function() {"
+    $(function () {
+
+      // Slideshow 1
+      $("#slider1").responsiveSlides({
+       
+        speed: 800
+      });
+
+     
+
+      // Slideshow 4
+      
+
+    });
